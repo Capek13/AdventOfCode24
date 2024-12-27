fe72e186944e332b4deeaf19a4790d62285eb681
Jaromír Cejpek <mira.cejpek@gmail.com>
2024-12-27 11:00:51 +0100

    day 1 complete

--- day1.py
+++ day1.py
@@ -7,8 +7,8 @@ for value in data:
     if len(splitedValue)== 2:
         if splitedValue[0].isnumeric() and splitedValue[1].isnumeric():
             x,y = splitedValue
-            col1.append(x)
-            col2.append(y)
+            col1.append(int(x))
+            col2.append(int(y))
         else:
             print("value x or y is not number")
     else:
@@ -16,5 +16,7 @@ for value in data:
 if type(col1) != str or type(col2) != str:
     col1.sort()
     col2.sort()
-
-
+result=0
+for i in range(0,len(col1)):
+    result = result + abs(col2[i]-col1[i]) 
+print(result)
