86ac67d14318845a4de4936e7f8d0b82a303931f
Jaromír Cejpek <mira.cejpek@gmail.com>
2024-12-27 11:11:28 +0100

    move get colls to the myFunctions

--- day1.py
+++ day1.py
@@ -1,18 +1,8 @@
 from myFunctions import MyFunctions
 print("start")
 data = MyFunctions.OpenFile("day1_input.txt")
-col1,col2 = [],[]
-for value in data:
-    splitedValue = value.split("   ")
-    if len(splitedValue)== 2:
-        if splitedValue[0].isnumeric() and splitedValue[1].isnumeric():
-            x,y = splitedValue
-            col1.append(int(x))
-            col2.append(int(y))
-        else:
-            print("value x or y is not number")
-    else:
-        print("In line is different count of values! Expected 2 values")
+col1,col2 = MyFunctions.GetNumberCollumns(data)
+
 if type(col1) != str or type(col2) != str:
     col1.sort()
     col2.sort()
--- myFunctions.py
+++ myFunctions.py
@@ -5,3 +5,22 @@ class MyFunctions():
         list = f.read().split("\n")
         return list
     
+    # in must be fix for more collumns
+    def GetNumberCollumns(data):
+        col1,col2 = [],[]
+        collumnsCount = -1
+        for value in data:
+            splitedValue = value.split("   ")
+            if collumnsCount < 0:
+                collumnsCount = len(splitedValue)
+            if len(splitedValue)== 2:
+                if splitedValue[0].isnumeric() and splitedValue[1].isnumeric():
+                    x,y = splitedValue
+                    col1.append(int(x))
+                    col2.append(int(y))
+                else:
+                    print("value x or y is not number")
+            else:
+                print(f"In line is different count of values! Expected first line value values count {0}", collumnsCount)
+        return
+    
