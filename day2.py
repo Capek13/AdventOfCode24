536b02d6f7fb61b73a192cba860f28b860bd3300
Jaromír Cejpek <mira.cejpek@gmail.com>
2025-01-04 10:52:00 +0100

    Day2-Complete

--- day2.py
+++ day2.py
@@ -1,101 +1,54 @@
 from myFunctions import MyFunctions
 print("start")
 data = MyFunctions.OpenFile("day2_input.txt")
+#data = MyFunctions.OpenFile("day2_dataSample.txt")
 data = MyFunctions.GetListsList(data)
 safeCount = 0 # result
 
 def CompareDifference(value1, value2):
-    difference = abs(value1 - value2)
-    if difference > 3 or difference == 0 :
-        return False
-    return True
+    if 0 < abs(value1 - value2) < 4 :
+        return 1
+    else:
+        return 0
 
 
-def CheckSteps(value1,value2,state):
-    if state == "decreasing":
-        if value1 - value2 > 0:
-            return True
-    elif state == "increasing":
-        if value1 - value2 < 0:
-            return True
-    return False
+def CheckSteps(value0,value1,value2):
+    if value0 - value1 > 0 and value1 - value2 > 0:
+        return 1
+    elif value0 - value1 < 0 and value1 - value2 < 0:
+        return 1
+    else:
+        return 0
 
 def ForeachValues(values,toleranceNotUsed):
-    state = ""
-    if toleranceNotUsed == False:
-        print(values)
-    if len(values) > 1:
-        if values[0]-values[1] > 0:
-            state = "decreasing"
-        elif values[0]-values[1] < 0:
-            state = "increasing"
-        else:
-            if toleranceNotUsed:
-                print(values , " CHECK 1")
-                return ForeachValues(values[1:],False)
-            else:
-                return 0
-        
     lastIndex = len(values)-1
     for i in range(0, lastIndex):
                 ii = i+1
                 if CompareDifference(values[i],values[ii]) == False:
                     if toleranceNotUsed:
-                        print(values, " COMPARE 2")
-                        if i == lastIndex-1:
+                        if ForeachValues(values[:ii]+values[ii+1:],False):
                             return 1
                         else:
-                            if ForeachValues(values[:ii]+values[ii+1:],False):
-                                return 1
-                            else:
-                                if ForeachValues(values[:i]+values[i+1:],False):
-                                    return 1
-                                else:
-                                    if i > 0:
-                                        print(i, "i")
-                                        return ForeachValues(values[:i-1]+values[i:],False)
+                            return ForeachValues(values[:i]+values[i+1:],False)
                     else:
                         return 0
-                            
-                if CheckSteps(values[i],values[ii],state)==False:
-                    if toleranceNotUsed:
-                        print(values , "CHECK 3")
-                        if i == lastIndex-1:
-                            return 1
-                        else:
+                if i > 0:            
+                    if CheckSteps(values[i-1],values[i],values[ii])==False:
+                        if toleranceNotUsed:
                             if ForeachValues(values[:ii]+values[ii+1:],False):
                                 return 1
                             else:
                                 if ForeachValues(values[:i]+values[i+1:],False):
                                     return 1
                                 else:
-                                    if i > 0:
-                                        if ForeachValues(values[:i-1]+values[i:],False):
-                                            return 1
-                                        else:
-                                            if i==2:
-                                                return ForeachValues(values[1:],False)
-                                            else:
-                                                return 0
-                                    else:
-                                        return 0
-                    else:           
-                        return 0
-
+                                    return ForeachValues(values[:i-1]+values[i:],False)
+                        else:           
+                            return 0
                 if i == lastIndex-1:
-                    if toleranceNotUsed == False:
-                        print("END")
                     return 1
-
-
+                
 for values in data:
-    toleranceNotUsed = True
-    #safeCount += ForeachValues(values,toleranceNotUsed)
-    result = ForeachValues(values,toleranceNotUsed)
-    safeCount += result
-    #if result:
-       # print(values)
+    toleranceNotUsed = True # For 1part change this value to False
+    safeCount += ForeachValues(values,toleranceNotUsed)
         
-        
-
 print(f"Save reports was found: {safeCount}")
