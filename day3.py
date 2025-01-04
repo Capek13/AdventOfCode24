afc4eff243fe5dcc82ae35c6bbed73b67dcb6e79
Jaromír Cejpek <mira.cejpek@gmail.com>
2025-01-04 22:26:56 +0100

    day3-first part

--- day3.py
+++ day3.py
@@ -8,6 +8,5 @@ text="".join(data)
 text= text.replace(" ","")
 
 values = re.findall(r"mul\(\b([0-9]|[1-9][0-9]{1,2})\b,\b([0-9]|[1-9][0-9]{1,2})\b\)",text)
-print(values)
-myList = map(lambda x,y: int(x)*int(y), values)
-print(list(myList))
+myList = list(map(lambda pair: int(pair[0])*int(pair[1]), values))
+print(sum(myList))
