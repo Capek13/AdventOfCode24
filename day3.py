6a914a8a607d4a0a0a84dd44b452e7f30795ae91
Jaromír Cejpek <mira.cejpek@gmail.com>
2025-01-06 22:38:10 +0100

    Day3 COMPLETE

--- day3.py
+++ day3.py
@@ -12,12 +12,14 @@ print(sum(myList))
 mulValues = list(re.finditer(r"mul\(\b([0-9]|[1-9][0-9]{1,2})\b,\b([0-9]|[1-9][0-9]{1,2})\b\)",text))
 doValues = list(re.finditer(r"do\(\)",text))
 dontValues = list(re.finditer(r"don't\(\)",text))
+
 startIndex = 0
 iDo=0
 iDont=0 
 activIntervals = []
+
 while iDo < len(doValues)-1 or iDont < len(dontValues)-1:
-    if doValues[iDo].start() < dontValues[iDont].start() and iDo < len(doValues):
+    if doValues[iDo].start() < dontValues[iDont].start():
         if startIndex < 0:
             startIndex = doValues[iDo].start()
         if iDo < len(doValues)-1:
@@ -28,21 +30,19 @@ while iDo < len(doValues)-1 or iDont < len(dontValues)-1:
             startIndex = -1
         if iDont < len(dontValues)-1:
             iDont += 1
-    if iDo == len(doValues)-1 and iDont == len(dontValues)-1 and  startIndex >= 0:
-            print("I need be here!!!")
-            activIntervals.append([startIndex,-1])
+
+if doValues[-1].start() > dontValues[-1].start():
+        activIntervals.append([doValues[-1].start(),-1])
+
 myMulValues = []
 for value in mulValues:
-    for i in range(0,len(activIntervals)-1):
-        if activIntervals[i][0] < value.start() < activIntervals[i][1]:
+    for i in range(0,len(activIntervals)):
+        if activIntervals[i][0] < value.start() < activIntervals[i][1] or activIntervals[i][1] == -1 and activIntervals[i][0] < value.start():
             myMulValues.append(value.group())
             break
 
 myMulValues = "".join(myMulValues)
 mulValues = re.findall(r"mul\(\b([0-9]|[1-9][0-9]{1,2})\b,\b([0-9]|[1-9][0-9]{1,2})\b\)",myMulValues)
 myList = list(map(lambda pair: int(pair[0])*int(pair[1]), mulValues))
-print(sum(myList))
 
-#[print(value.group(), value.start(), value.end()) for value in mulValues]
-#[print(value.group(), value.start(), value.end()) for value in doValues]
-#[print(value.group(), value.start(), value.end()) for value in dontValues]
+print(sum(myList))
